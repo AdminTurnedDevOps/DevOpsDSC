f615d192eebf9c6eaa4015621c1cbda227dd0489
Mike <mlevan1992@gmail.com>
2018-02-24 10:34:18 -0500

    Create a config for Win updates that include Security + Important patches

---
+++ ScheduleWinUpdateInstallDSC.ps1
@@ -0,0 +1,24 @@
+Configuration ScheduleWinUpdateInstallDSC
+{
+    Import-DscResource -ModuleName xWindowsUpdate
+
+    Node localhost
+    {
+
+        xWindowsUpdateAgent MuSecurityImportant
+        {
+            
+        IsSingleInstance = 'Yes'
+        UpdateNow        = $false
+        Category         = @('Security,Important')
+        Source           = 'WindowsUpdate'
+        Notifications    = 'ScheduledInstallation'
+
+        }
+
+    }
+
+}
+
+ScheduleWinUpdateInstallDSC
+Start-DscConfiguration -ComputerName localhost -Path C:\ScheduleWinUpdateInstallDSC -Wait -Force
